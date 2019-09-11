291fc07c002260eb2b93cb1baee031f3e5a9fdd9
Catherine Stewart <catherine.c.stwrt@gmail.com>
2019-09-11 15:34:32 -0700

    model scaffold

---
+++ schemes/scheme-model.js
@@ -0,0 +1,20 @@
+function find() {}
+
+function findById(id) {}
+
+function findSteps(id) {}
+
+function add(scheme) {}
+
+function update(changes, id) {}
+
+function remove(id) {}
+
+module.exports = {
+  find,
+  findById,
+  findSteps,
+  add,
+  update,
+  remove
+};
